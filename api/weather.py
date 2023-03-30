0fc03d65c54d023c48ef6910eca5c9a9447655ee
rjrobben <rayipwm@gmail.com>
2023-03-30 21:24:04 +0800

    init commit, flask setup and weather class

---
+++ api/weather.py
@@ -0,0 +1,61 @@
+from flask import jsonify
+import requests
+from datetime import datetime, timedelta
+
+class Weather:
+
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.weather_url = 'https://api.openweathermap.org/data/2.5/weather'
+        self.forecast_url = 'https://api.openweathermap.org/data/2.5/forecast'
+
+    def get_current_weather(self, location: str):
+        # make a request to the OpenWeatherMap API
+        url = f'{self.weather_url}?q={location}&appid={self.api_key}&units=metric'
+        response = requests.get(url)
+        data = response.json()
+
+        # parse the response and extract relevant weather information
+        if data['cod'] != 200:
+            return jsonify({'error': data['message']}), data['cod']
+
+        current_temp = data['main']['temp']
+        humidity = data['main']['humidity']
+        weather_description = data['weather'][0]['description']
+
+        # construct the response object with the weather data and forecast
+        response_data = {
+            'location': location,
+            'current_weather': {
+                'temperature': current_temp,
+                'humidity': humidity,
+                'weather_description': weather_description
+            },
+        }
+
+        return jsonify(response_data)
+
+    def get_forecast(self, location: str):
+        # make a request to the OpenWeatherMap API for the forecast
+        url = f'{self.forecast_url}?q={location}&appid={self.api_key}&units=metric'
+        response = requests.get(url)
+        data = response.json()
+
+        # parse the response and extract the forecast information
+        forecasts = {} 
+        for item in data['list']:
+            dt = datetime.fromisoformat(item['dt_txt'])
+            date = dt.date().strftime('%Y-%m-%d')
+            if date not in forecasts:
+                forecasts[date] = {
+                    'datetime': dt.isoformat(),
+                    'temp': item['main']['temp'],
+                    'humidity': item['main']['humidity'],
+                    'weather_description': item['weather'][0]['description']
+                }
+
+        response_data = {
+            'location': location,
+            'forecast': forecasts
+        }
+        return jsonify(response_data)
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from api.weather import Weather
+
+app = Flask(__name__)
+
+api_key = 'f819e16273820cd7df6d1bfee66a7bda'
+weather = Weather(api_key)
+print(weather)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/weather')
+def get_weather():
+    # get the location from the query parameter
+    location = request.args.get('location')
+    if not location:
+        return jsonify({'error': 'location parameter is missing'}), 400
+    
+    return weather.get_current_weather(location)
+
+
+@app.route('/forecast')
+def get_forecast():
+    # get the location from the query parameter
+    location = request.args.get('location')
+    if not location:
+        return jsonify({'error': 'location parameter is missing'}), 400
+
+    return weather.get_forecast(location)
+
+ 
